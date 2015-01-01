dd2a7ed913d454aa9b3c18b55ae21368c349450c
unknown <nickbrons@msn.com>
2015-01-01 16:13:11 -0700

    Cleaned up code for A and B

--- A.java
+++ A.java
@@ -1,3 +1,4 @@
+/* Question: http://www.acmgnyr.org/year2008/a.pdf */
 package ACM2008;
 
 import java.util.Scanner;
@@ -7,28 +8,30 @@ public class A {
 	public static void main(String args[]){
 		Scanner in = new Scanner (System.in);
 		
-		int T = in.nextInt();
+		int N = in.nextInt();	//the number of cases
 		
-		for(int t=0; t<T; t++){
-			int a = in.nextInt();
+		for(int t=0; t<N; t++){
+			int a = in.nextInt();	//two positive integers
 			int b = in.nextInt();
 			
-			System.out.println((t+1)+" "+lcm(a,b)+" "+GCF(a,b));
+			System.out.println((t+1)+" "+LCM(a,b)+" "+GCF(a,b));		//uses separate functions to get a print out the lcm and gcf
 		
-		}		
+		}
+		
+		in.close();
 		
 	}
 
-	public static int GCF(int a, int b){
+	public static int GCF(int a, int b){	//Euclidean algorithm
 		while(b>0){
-			int temp =b;
-			b = a%b;
-			a=temp;
+			int temp =b;					//stores b in a temporary variable
+			b = a%b;						//b is the modulus of a div b
+			a=temp;							//a is b on the previous iteration
 		}
-		return a;
+		return a;							//returns a when b==0 
 	}
 	
-	public static int lcm (int a, int b){
+	public static int LCM (int a, int b){	//LCM uses the GCF, is just one of the numbers multipled by (the other number divided by the GCF of the two numbers)
 		return a*(b/GCF(a,b));
 	}
 }
--- B.java
+++ B.java
@@ -1,7 +1,6 @@
+/*Question: http://www.acmgnyr.org/year2008/b.pdf */
 package ACM2008;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class B {
@@ -14,43 +13,43 @@ public class B {
 		
 		for(int t=0; t<T; t++){
 			in.nextLine();
-			char[] word = in.nextLine().toCharArray();
-			char[] kalpha = in.next().toCharArray();
-			char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-			char[] finished = new char[word.length];
+			char[] word = in.nextLine().toCharArray();				//sends the phrase to a character array
+			char[] kalpha = in.next().toCharArray();				//sends scrambled alphabet to character array
+			char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();	//creates a character array of the alphabet
+			char[] finished = new char[word.length];				//empty character array for the unscrambled word
 			  
 			
-			  for(int i =0; i<word.length; i++){
-					int n = Check(word[i],alpha);
-					if(n==-1)
-						finished[i]=' ';
+			  for(int i =0; i<word.length; i++){					//loop iterates the input phrase
+					int n = Check(word[i],alpha);					//n is the index of the first character
+					if(n==-1)										//if the index is -1, the character is a space
+						finished[i]=' ';							
 					else{
-					finished[i] = kalpha[n];
+					finished[i] = kalpha[n];						//otherwise the character is located at the index in the scrambled alphabet
 					
-					}//System.out.println(kalpha);
+					}
 			  }
-			  System.out.print((t+1)+" ");
-				for(int i =0; i<finished.length; i++){
+			  System.out.print((t+1)+" ");							//prints case number
+				for(int i =0; i<finished.length; i++){				//loops to print out each character of the finished word
 					System.out.print(finished[i]);
 				}
 				
-				System.out.println();
+				System.out.println();								//prints a new line
 	
-		}	
+		}
+		
+		in.close();
 		
 	}
 	
 	public static int Check(char a, char[] alpha){
-		int i=0;
-		if(a==' '){
+		int i=0;								//the counter is 0 by default
+		if(a==' ')							//if the character is a space, returns -1 as the "index"
 			return -1;
+	for(i=0; i<alpha.length; i++){			//otherwise loops through the alphabet until the character finds a match
+			if(a==alpha[i])					//breaks the loop
+				break;
 		}
-		for(i=0; i<alpha.length; i++){
-			if(a==alpha[i]){
-				return i;
-			}
-		}
-		return i;
+		return i;							//returns the index in the alphabet
 	}
 
 }
